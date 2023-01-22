f91244db7a12792755780682b3117ed1200b9806
sainamercy <sainamercy@gmail.com>
2023-01-22 17:38:53 +0300

    updated styles

--- src/components/BotList.jsx
+++ src/components/BotList.jsx
@@ -2,22 +2,20 @@ import React from "react";
 
 function BotList({name, category, image, phrase, damage, health, armor }){
  return <div className="flex justify-center p-3 mt-4">
- <div className="rounded-lg shadow-lg bg-white w-60">
+ <div className="rounded-lg shadow-lg bg-lime-100 w-60">
    <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
      <img className="rounded-t-lg" src={image} alt={name}/>
    </a>
-   <div className="p-6">
-     <div className="text-gray-900 flex justify-between mb-2 font-medium">
-     <h5 className="text-xl">Name: {name}</h5>
-     <h6 className="text-xl">class: {category}</h6>
+   <div className="p-4">
+     <div className="text-gray-900 mb-2 font-medium">
+     <h5 className="text-2xl">{name}</h5>
+     <h6 className="text-xl">"{category}"</h6>
      </div>
-     <p className="text-gray-700 mb-4 overflow-hidden">
-       {phrase}
-     </p>
+     <p className="text-gray-700 mb-4 break-words">{phrase}</p>
      <div className="flex justify-between">
-        <p>damage: {damage}</p>
-        <p>health: {health}</p>
-        <p>armor: {armor}</p>
+        <p>💔: {damage}</p>
+        <p>⚡: {health}</p>
+        <p>🛡️: {armor}</p>
      </div>
    </div>
  </div>
